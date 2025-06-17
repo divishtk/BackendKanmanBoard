4f7df90150b85d35166f0b6a0a545962c07cb2e7
divishtk <divisht.avion@hotmail.com>
2025-06-17 20:21:18 +0530

    Refactor note routes to improve permission validation and code consistency

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,15 +1,27 @@
-import { Router } from "express";
-import { USER_ROLES_ENUM } from "../utils/constants";
-import { validateProjectPermission } from "../middlewares/verifyJwt.middleware";
+import { Router } from 'express';
+import { AvailableUserRoles, USER_ROLES_ENUM } from '../utils/constants.js';
+import { validateProjectPermission } from '../middlewares/verifyJwt.middleware.js';
+import {
+  createNote,
+  deleteNote,
+  getNotesById,
+  updateNote,
+} from '../controllers/note.controller.js';
 
 const router = Router();
 
-router.route('/:projectId')
-.get(validateProjectPermission([USER_ROLES_ENUM.ADMIN , USER_ROLES_ENUM.MEMBER]),getNotes)
-.post(validateProjectPermission([USER_ROLES_ENUM.ADMIN], USER_ROLES_ENUM.MEMBER),createNote) ;
+router
+  .route('/:projectId')
+  .get(validateProjectPermission(AvailableUserRoles), getNotes)
+  .post(validateProjectPermission([USER_ROLES_ENUM.ADMIN]), createNote);
 
+router
+  .route('/:projectId/n/:noteId')
+  .get(validateProjectPermission(AvailableUserRoles), getNotesById)
+  .put(validateProjectPermission([USER_ROLES_ENUM.ADMIN]), updateNote)
+  .delete(validateProjectPermission([USER_ROLES_ENUM.ADMIN]), deleteNote)
+  .post(validateProjectPermission([USER_ROLES_ENUM.ADMIN]), createNote);
 
 //router.route('').post() ;
 
-
-export default router  
+export default router;
